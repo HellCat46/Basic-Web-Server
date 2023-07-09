4b2ea59c7ed1a0eadea43efd590d1bac203db16d
HellCat46 <hellcat2388@gmail.com>
2023-07-09 18:01:30 +0530

    Add an option to choose root directory

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,46 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{BufRead, BufReader, Write};
 
 fn main() {
-    let args : Vec<_>= env::args().collect();
-    let mut port  = "3000";
-
-    for arg in args.iter() {
-        if arg.starts_with("--port") {
-            port = arg.split("=").last().unwrap().trim();
-        }
-    }
-
+    let (port, _) = get_args();
     let address = format!("127.0.0.1:{}", port);
     let listener = TcpListener::bind(&address).expect("Failed to start the server");
+
     println!("Listening on http://{}",address);
 
     for request in listener.incoming() {
         let mut stream = request.unwrap();
-
-        let path = handle_request(&stream);
-        let (mut content, status_code) = get_file(path);
+        
+        let (content, status_code) = get_file(handle_request(&stream));
         let mut response = format!(
-            "HTTP/1.1 {}\nContent-Length: {}\n\n",
+            "HTTP/2 {}\nContent-Length: {}\n\n",
             status_code,
             content.len()
-        )
-            .into_bytes();
-        response.append(&mut content);
-        //println!("{response}");
-        stream.write_all(&response).unwrap();
+        ).into_bytes();
 
+        response.extend(content);
+        stream.write_all(&response).unwrap();
     }
 
 }
 
+fn get_args() -> (String, String) {
+    let args : Vec<String> = env::args().collect();
+    let mut port = "3000";
+    let mut path = "";
+    for arg in args.iter() {
+    if arg.starts_with("--port") {
+        port = arg.split("=").last().unwrap().trim();
+    }
+    if arg.starts_with("--rootdir") {
+        path = arg.split("=").last().unwrap().trim();
+    }
+    }
+    (port.to_owned(), path.to_owned())
+}
+
 fn handle_request(mut stream : &TcpStream) -> String {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let http_request: Vec<String> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
@@ -55,15 +60,22 @@ fn handle_request(mut stream : &TcpStream) -> String {
 }
 
 fn get_file(path : String) -> (Vec<u8>, String ){
-    let full_path = "Data".to_string() + &path;
+    let mut full_path = path;
 
-    let content = std::fs::read(full_path);
+    let (_,rootdir) = get_args();
+    if rootdir.is_empty() {
+        full_path.remove(0);
+    }else {
+        full_path = format!("{}{}", rootdir, full_path)
+    }
+    
+    let content = std::fs::read(&full_path);
     match content {
         Ok(content) =>{
             (content, "200 OK".to_string())
         },
         Err(_) => {
-            match std::fs::read("../Data/3.html") {
+            match std::fs::read(format!("{}/404.html", full_path.split("/").nth(0).expect("Somehow it received null"))) {
                 Ok(content) => (content, "404 Not Found".to_string()),
                 Err(_) => (Vec::from("404 Not Found".as_bytes()), "404 Not Found".to_string())
             }
